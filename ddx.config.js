3d25c048b6f76e47039b31f87f414d80742e7d9a
谢仁洪 <renhong.xrh@alibaba-inc.com>
2019-11-13 14:53:17 +0800

    feat: 增加关于页面

--- ddx.config.js
+++ ddx.config.js
@@ -1,7 +1,12 @@
 export default {
   css: {
     sass: {
-      data: '$color: red;'
+      data: '$primary: fe7756;'
     }
-  }
+  },
+  transRpx: {
+    // 默认原生小程序不做转化
+    exclude: /\.acss$/,
+    designWidth: 375
+  },
 };
